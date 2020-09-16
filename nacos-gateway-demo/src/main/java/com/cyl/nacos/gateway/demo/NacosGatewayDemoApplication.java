52d86ab25835e403321eecea6cde5d21b4c1df12
chenyiliang <yiliang.chen@gocmx.com>
2020-09-16 12:12:53 +0800

    Get route from nacos config

--- nacos-gateway-demo/src/main/java/com/cyl/nacos/gateway/demo/NacosGatewayDemoApplication.java
+++ nacos-gateway-demo/src/main/java/com/cyl/nacos/gateway/demo/NacosGatewayDemoApplication.java
@@ -3,13 +3,22 @@ package com.cyl.nacos.gateway.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class NacosGatewayDemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(NacosGatewayDemoApplication.class, args);
+
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(NacosGatewayDemoApplication.class, args);
+
+        String userName = applicationContext.getEnvironment().getProperty("user.name");
+        String userAge = applicationContext.getEnvironment().getProperty("user.age");
+        System.err.println("user name:" + userName + "; age: " + userAge);
+
     }
 
 }
