4989ff707cb8213779ad12937c2b6724b98c4fd1
chenyiliang <yiliang.chen@gocmx.com>
2020-09-16 19:05:50 +0800

    Rocket MQ

---
+++ rocketmq-consumer-demo/src/main/java/com/cyl/rocketmq/consumer/demo/RocketmqConsumerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.cyl.rocketmq.consumer.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketmqConsumerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RocketmqConsumerDemoApplication.class, args);
+    }
+
+}
---
+++ rocketmq-provider-demo/src/test/java/com/cyl/rocketmq/provider/demo/RocketmqProviderDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyl.rocketmq.provider.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocketmqProviderDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
